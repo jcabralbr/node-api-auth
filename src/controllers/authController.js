16b1d756e7e2c2c350f204563ddf1509f255e1e3
Julio Cesar Cabral <jcabralbr@gmail.com>
2018-07-06 09:59:27 -0300

    creating authenticate route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
-router.post('/register', async(req, res) => {
+router.post('/register', async (req, res) => {
     const { email } = req.body;
     try {
-    if (await User.findOne({email})){
-        return res.status(400).send({error: 'User already exists'});
-    }
-
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ error: 'User already exists' });
+        }
 
         const user = await User.create(req.body);
-        return res.send({user});
+        user.password = undefined;
+        return res.send({ user });
     } catch (error) {
-        return res.status(400).send({error: 'Registration failed'});
+        return res.status(400).send({ error: 'Registration failed' });
     }
+});
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email}).select('+password');
+
+    if (!user){
+        return res.status(400).send({error:'User not found' })
+    }
+
+    if (!await bcrypt.compare(password, user.password)){
+        return res.status(400).send({error:'Invalid password' })
+    }
+    res.send({user});
 });
 
 module.exports = app => app.use('/auth', router);
