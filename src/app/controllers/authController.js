a787c8fe081a1dba92f744cc654da62b29b96930
Julio Cesar Cabral <jcabralbr@gmail.com>
2018-07-20 12:15:24 -0300

    Creating forgot password route

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const User = require('../models/user');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth')
+const crypto = require("crypto");
+
+const User = require('../models/user');
+const authConfig = require('../../config/auth');
+const mailer = require('../../modules/mailer');
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -37,6 +40,42 @@ router.post('/authenticate', async (req, res) => {
     user.password = undefined;
     
     res.send({user, token: generateToken({id: user.id})});
+});
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user){
+            res.status(400).send({error: 'User not found.'})
+        }
+
+        const token = crypto.randomBytes(20).toString('hex');
+        let now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now
+            }
+        })
+
+        mailer.sendMail({
+            to: email,
+            from: 'julio@gmail.com',
+            template: 'auth/forgot_password',
+            context: { token }
+        }, (err) => {
+            if (err){
+                return res.status(400).send({error: 'Cannot send forgot password email.'})
+            }
+            return res.send();    
+        })
+    } catch (error) {
+        res.status(400).send({error: 'Error on forgot password, try again!'})
+    }
+
 });
 
 function generateToken(params = []){
